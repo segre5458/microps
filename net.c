3b96a4ed088a145016b844f789b39ab8cf0ec694
segre <segrehouck5458@gmail.com>
2024-02-11 16:25:41 +0900

    step2

--- net.c
+++ net.c
@@ -110,6 +110,12 @@ int net_run(void)
 {
     struct net_device *dev;
 
+    if (intr_run() == -1)
+    {
+        errorf("intr_run() failure");
+        return -1;
+    }
+
     debugf("open all devices...");
     for (dev = devices; dev; dev = dev->next)
     {
@@ -128,12 +134,17 @@ void net_shutdown(void)
     {
         net_device_close(dev);
     }
+    intr_shutdown();
     debugf("shutting down");
 }
 
-int 
-net_init(void)
+int net_init(void)
 {
+    if (intr_init() == -1)
+    {
+        errorf("intr_init() failure");
+        return -1;
+    }
     infof("initialized");
     return 0;
 }
